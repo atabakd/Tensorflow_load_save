5113c60a39bfb4e4a84cdc7d7a1686e31cf89e15
Atabak <atabakd@gmail.com>
2016-06-17 18:29:24 +0100

    remove junk

--- LinearRegression.py
+++ LinearRegression.py
@@ -18,8 +18,6 @@ class linearRegression(object):
         self._create_network()     
         tf.initialize_all_variables().run()
         self._create_loss_optimizer()
-#        init = tf.initialize_all_variables()
-        
         
     def _create_network(self):
         self.w = tf.Variable(0.0, name="weights")
@@ -31,35 +29,6 @@ class linearRegression(object):
         self.optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(self.cost)
     
     def fit(self, x,y):
-        
-#        self.saver = tf.train.Saver()
         opt, cost= self.sess.run((self.optimizer, self.cost), \
                                   feed_dict={self.x:x,self.y:y})
                                   
-#    def save(self, checkpoint_dir, step):
-#        model_name = "LinearReg.model"
-##        model_dir = "%s_%s" % (self.dataset_name, self.batch_size)
-##        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
-#        model_dir = '/model'
-#
-#        if not os.path.exists(checkpoint_dir):
-#            os.makedirs(checkpoint_dir)
-#
-#        self.saver.save(self.sess,
-#                        os.path.join(model_dir, model_name),
-#                        global_step=step)
-#
-#    #LOAD MODEL
-#    def load(self, checkpoint_dir):
-#        print(" [*] Reading checkpoints...")
-#
-##        model_dir = "%s_%s" % (self.dataset_name, self.batch_size)
-##        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
-#        model_dir = '/model'
-#        ckpt = tf.train.get_checkpoint_state(model_dir)
-#        if ckpt and ckpt.model_checkpoint_path:
-#            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-#            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
-#            return True
-#        else:
-#            return False
